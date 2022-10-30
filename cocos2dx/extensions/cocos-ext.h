82ded4fa388f3a0cff2083eb5e9e5845720aabf1
altalk23 <45172705+altalk23@users.noreply.github.com>
2022-10-30 16:28:55 +0300

    more cleanup

---
+++ cocos2dx/extensions/cocos-ext.h
@@ -0,0 +1,11 @@
+#ifndef __COCOS2D_EXT_H__
+#define __COCOS2D_EXT_H__
+
+#include "ExtensionMacros.h"
+
+#include "CCControlExtension/CCControlExtensions.h"
+
+#include "network/HttpRequest.h"
+#include "network/HttpResponse.h"
+#include "network/HttpClient.h"
+
